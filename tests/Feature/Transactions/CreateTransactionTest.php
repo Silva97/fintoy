61c84c4c985e28cd3dbc23e8603c6942dd7da862
Luiz Felipe <felipe.silva337@yahoo.com>
2024-02-28 17:37:34 -0300

    test: add test cases to POST /transactions endpoint

---
+++ tests/Feature/Transactions/CreateTransactionTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Feature\Transactions;
+
+use App\Models\User;
+use Tests\TestCase;
+
+class CreateTransactionTest extends TestCase
+{
+    public function test_make_transaction_between_common_users()
+    {
+        /** @var User */
+        $payer = User::factory()->create();
+        $payee = User::factory()->create();
+
+        $this->actingAs($payer);
+
+        $this->postJson('/transactions', [
+            'payee_id' => $payee->id,
+            'value' => 100,
+        ])
+            ->assertNoContent();
+
+        $payer->wallet->refresh();
+        $payee->wallet->refresh();
+
+        $this->assertEquals(8901, $payer->wallet->balance);
+        $this->assertEquals(9101, $payee->wallet->balance);
+    }
+
+    public function test_make_transaction_from_common_user_to_shopkeeper()
+    {
+        /** @var User */
+        $payer = User::factory()->create();
+        $payee = User::factory()->shopkeeper()->create();
+
+        $this->actingAs($payer);
+
+        $this->postJson('/transactions', [
+            'payee_id' => $payee->id,
+            'value' => 100,
+        ])
+            ->assertNoContent();
+
+        $payer->wallet->refresh();
+        $payee->wallet->refresh();
+
+        $this->assertEquals(8901, $payer->wallet->balance);
+        $this->assertEquals(9101, $payee->wallet->balance);
+    }
+
+    public function test_try_to_make_transaction_as_shopkeeper_expects_403()
+    {
+        /** @var User */
+        $payer = User::factory()->shopkeeper()->create();
+        $payee = User::factory()->create();
+
+        $this->actingAs($payer);
+
+        $this->postJson('/transactions', [
+            'payee_id' => $payee->id,
+            'value' => 100,
+        ])
+            ->assertForbidden();
+
+        $payer->wallet->refresh();
+        $payee->wallet->refresh();
+
+        $this->assertEquals(9001, $payer->wallet->balance);
+        $this->assertEquals(9001, $payee->wallet->balance);
+    }
+
+    public function test_try_to_make_transaction_with_insufficient_balance_expects_422()
+    {
+        /** @var User */
+        $payer = User::factory()->create();
+        $payee = User::factory()->create();
+
+        $this->actingAs($payer);
+
+        $this->postJson('/transactions', [
+            'payee_id' => $payee->id,
+            'value' => 9002,
+        ])
+            ->assertUnprocessable([
+                'errors' => [
+                    'value' => ['The value exceeds current user balance.'],
+                ],
+            ]);
+
+        $payer->wallet->refresh();
+        $payee->wallet->refresh();
+
+        $this->assertEquals(9001, $payer->wallet->balance);
+        $this->assertEquals(9001, $payee->wallet->balance);
+    }
+}

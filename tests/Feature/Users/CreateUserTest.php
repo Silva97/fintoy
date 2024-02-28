500585aa6e4486ee3ee0304b8416cafeae911fe8
Luiz Felipe <felipe.silva337@yahoo.com>
2024-02-27 23:50:04 -0300

    test: add test cases to POST /users endpoint

---
+++ tests/Feature/Users/CreateUserTest.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace Tests\Feature\Users;
+
+use App\Models\User;
+use App\Models\Wallet;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateUserTest extends TestCase
+{
+    use WithFaker;
+
+    public function test_create_new_common_user()
+    {
+        $userData = [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'is_shopkeeper' => false,
+            'identification_number' => $this->faker->numerify('###########'),
+            'password' => 'password123456',
+        ];
+
+        $response = $this->postJson('/users', $userData);
+
+        $response
+            ->assertCreated()
+            ->assertExactJson([
+                'id' => $response->json('id'),
+                'name' => $userData['name'],
+                'email' => $userData['email'],
+                'is_shopkeeper' => $userData['is_shopkeeper'],
+                'identification_number' => $userData['identification_number'],
+            ]);
+
+        $user = User::find($response->json('id'));
+        $this->assertNotNull($user);
+
+        $this->assertNotNull($user->wallet);
+        $this->assertEquals(0, $user->wallet->balance);
+    }
+
+    public function test_create_new_shopkeeper_user()
+    {
+        $userData = [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'is_shopkeeper' => true,
+            'identification_number' => $this->faker->numerify('##############'),
+            'password' => 'password123456',
+        ];
+
+        $response = $this->postJson('/users', $userData);
+
+        $response
+            ->assertCreated()
+            ->assertExactJson([
+                'id' => $response->json('id'),
+                'name' => $userData['name'],
+                'email' => $userData['email'],
+                'is_shopkeeper' => $userData['is_shopkeeper'],
+                'identification_number' => $userData['identification_number'],
+            ]);
+
+        $user = User::find($response->json('id'));
+        $this->assertNotNull($user);
+
+        $this->assertNotNull($user->wallet);
+        $this->assertEquals(0, $user->wallet->balance);
+    }
+
+    public function test_create_user_with_duplicated_email_expects_422()
+    {
+        $userData = [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'is_shopkeeper' => false,
+            'identification_number' => $this->faker->numerify('###########'),
+            'password' => 'password123456',
+        ];
+
+        $this->postJson('/users', $userData)
+            ->assertCreated();
+
+        $userData['identification_number'] = $this->faker->numerify('###########');
+
+        $this->postJson('/users', $userData)
+            ->assertUnprocessable()
+            ->assertJson([
+                'errors' => [
+                    'email' => ['The email has already been taken.'],
+                ],
+            ]);
+    }
+
+    public function test_create_user_with_duplicated_id_number_expects_422()
+    {
+        $userData = [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'is_shopkeeper' => false,
+            'identification_number' => $this->faker->numerify('###########'),
+            'password' => 'password123456',
+        ];
+
+        $this->postJson('/users', $userData)
+            ->assertCreated();
+
+        $userData['email'] = $this->faker->unique()->safeEmail();
+
+        $this->postJson('/users', $userData)
+            ->assertUnprocessable()
+            ->assertJson([
+                'errors' => [
+                    'identification_number' => ['The identification number has already been taken.'],
+                ],
+            ]);
+    }
+}
